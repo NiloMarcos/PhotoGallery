1dcbd96027db39f7a5a64bc4b4b98dba87859111
Nilo Marcos <nilo.n2m15@gmail.com>
2021-11-26 14:09:10 -0300

    add styling for button

--- src/components/ButtonPagination/index.js
+++ src/components/ButtonPagination/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import './button.scss';
+
 export default function ButtonPagination({ setCurrentPage, pages }){
   return(
-    <p>{Array.from(Array(pages), (item, index) => {
-      return <button value={index} onClick={(event) => setCurrentPage(event.target.value)}>{index + 1}</button>
-    })}</p>
+    <div className="Button-div">
+      <p>{Array.from(Array(pages), (item, index) => {
+        return <button className="Button-pagination"value={index} onClick={(event) => setCurrentPage(event.target.value)}>{index + 1}</button>
+      })}</p>
+    </div>
   );
 }
